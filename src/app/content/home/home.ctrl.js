8bb85f35176f2d228b0ace304da9db26173d00e4
loljoho <jho1290@smail.raritanval.edu>
2015-06-15 14:58:43 -0400

    Layout dual sidenavs

--- src/app/content/home/home.ctrl.js
+++ src/app/content/home/home.ctrl.js
@@ -6,8 +6,89 @@ angular.module('johoApp')
 
 /* @ngInject */
 function Home() {
+  var self = this;
+
+  // Resume Download Links
+  self.links = [{
+    text: 'Google Drive',
+    icon: 'assets/images/logos/drive_icon_48px.png',
+    link: 'https://drive.google.com/folderview?id=0B9VAfEqSVO-MfnRweHFQT1dfNHRtaFpkM0J3ZW15b2FBbnhJQVJ4ZEc5OWpSRHEzSl92ZWM&usp=sharing'
+  }, {
+    text: 'Dropbox',
+    icon: 'assets/images/logos/dropbox_icon_48px.png',
+    link: 'https://www.dropbox.com/sh/nr5z44oom47pkfm/AACxoIgNtW_q0nHIOrcPZTb_a?dl=0'
+  }, {
+    text: 'Microsoft OneDrive',
+    icon: 'assets/images/logos/onedrive_icon_48px.png',
+    link: 'http://1drv.ms/1dGkysf'
+  }];
   
   
+
+  // Areas of Expertise
+  self.areas = [{
+    text: 'HTML5/CSS3/JS',
+    icon: 'assets/images/thumbs/dhtml.png',
+    rank: 100
+  }, {
+    text: 'AngularJS',
+    icon: 'assets/images/thumbs/angular.png',
+    rank: 98
+  }, {
+    text: 'jQuery, Ajax',
+    icon: 'assets/images/thumbs/jquery.png',
+    rank: 96
+  }, {
+    text: 'Node.js/npm',
+    icon: 'assets/images/thumbs/node.png',
+    rank: 93
+  }, {
+    text: 'Apache-MySQL-PHP',
+    icon: 'assets/images/thumbs/amp.png',
+    rank: 91
+  }, {
+    text: 'Bower',
+    icon: 'assets/images/thumbs/bower.png',
+    rank: 90
+  }, {
+    text: 'Responsive Design',
+    icon: 'assets/images/thumbs/rwd.png',
+    rank: 85
+  }, {
+    text: 'Git',
+    icon: 'assets/images/thumbs/git.png',
+    rank: 83
+  }, {
+    text: 'Amazon Web Services',
+    icon: 'assets/images/thumbs/aws.png',
+    rank: 82
+  }, {
+    text: 'Gulp/Grunt',
+    icon: 'assets/images/thumbs/gulp.png',
+    rank: 80
+  }, {
+    text: 'Sass/Scss',
+    icon: 'assets/images/thumbs/sass.png',
+    rank: 80
+  }, {
+    text: 'Data Structures',
+    icon: 'assets/images/thumbs/data.png',
+    rank: 76
+  }, {
+    text: 'Laravel/Lumen',
+    icon: 'assets/images/thumbs/laravel.png',
+    rank: 70
+  }, {
+    text: 'Python',
+    icon: 'assets/images/thumbs/python.png',
+    rank: 62
+  }, {
+    text: 'Java, C/C++/C#',
+    icon: 'assets/images/thumbs/java.png',
+    rank: 60
+  }];
+
+
 }
 
 })();
--- src/app/core/config/icons.config.js
+++ src/app/core/config/icons.config.js
@@ -9,19 +9,19 @@ function configureIcons($mdIconProvider) {
   $mdIconProvider
 
     // Menu Icons
-    .icon('face',       'assets/icons/md/face.svg'    )
-    .icon('code',       'assets/icons/md/code.svg'    )
-    .icon('school',     'assets/icons/md/school.svg'  )
-    .icon('phone',      'assets/icons/md/phone.svg'   )
-    .icon('work',       'assets/icons/md/work.svg'    )
-    .icon('home',       'assets/icons/md/home.svg'    )
-    .icon('mail',       'assets/icons/md/mail.svg'    )
+    .icon('face',           'assets/icons/md/face.svg'    )
+    .icon('code',           'assets/icons/md/code.svg'    )
+    .icon('school',         'assets/icons/md/school.svg'  )
+    .icon('phone',          'assets/icons/md/phone.svg'   )
+    .icon('work',           'assets/icons/md/work.svg'    )
+    .icon('home',           'assets/icons/md/home.svg'    )
+    .icon('mail',           'assets/icons/md/mail.svg'    )
 
     // Navigation Icons
-    .icon('menu-vert',  'assets/icons/md/menu-vert.svg')
-    .icon('menu-hori',  'assets/icons/md/menu-hori.svg')
-    .icon('menu-bars',  'assets/icons/md/menu-bars.svg')
-    .icon('menu-grid',  'assets/icons/md/menu-grid.svg')
+    .icon('menu-vert',      'assets/icons/md/menu-vert.svg')
+    .icon('menu-hori',      'assets/icons/md/menu-hori.svg')
+    .icon('menu-bars',      'assets/icons/md/menu-bars.svg')
+    .icon('menu-grid',      'assets/icons/md/menu-grid.svg')
 
     // Directional Icons
     .icon('arrow-left',     'assets/icons/md/arrow-left.svg'    )
@@ -29,11 +29,16 @@ function configureIcons($mdIconProvider) {
     .icon('chevron-left',   'assets/icons/md/chevron-left.svg'  )
     .icon('chevron-right',  'assets/icons/md/chevron-right.svg' )
 
+    // Cloud Storage Icons
+    .icon('gdrive',         'assets/icons/dl/drive_icon_48px.svg'   )
+    .icon('dropbox',        'assets/icons/dl/dropbox_icon_48px.svg' )
+    .icon('onedrive',       'assets/icons/dl/onedrive_icon_48px.svg')
+
     // Profile/Social Media
-    .icon('github',     'assets/icons/fa/github.svg'    )
-    .icon('linkedin',   'assets/icons/fa/linkedin.svg'  )
-    .icon('codepen',    'assets/icons/fa/codepen.svg'   )
-    .icon('angellist',  'assets/icons/fa/angellist.svg' );
+    .icon('github',         'assets/icons/fa/github.svg'    )
+    .icon('linkedin',       'assets/icons/fa/linkedin.svg'  )
+    .icon('codepen',        'assets/icons/fa/codepen.svg'   )
+    .icon('angellist',      'assets/icons/fa/angellist.svg' );
 
 }
 
